8d010e29e6191b10e21a9a56614fe39fc5497c79
Miljan Aleksic <miljan@aleksic.es>
2018-02-09 01:19:17 +0800

    reubicate dropdown nav component

--- packages/vuikit/src/core/nav/index.js
+++ packages/vuikit/src/core/nav/index.js
@@ -1,4 +1,5 @@
 export { default as Nav } from './nav'
+export { default as NavDropdown } from './nav-dropdown'
 export { default as NavItem } from './nav-item'
 export { default as NavItemHeader } from './nav-item-header'
 export { default as NavItemParent } from './nav-item-parent'
